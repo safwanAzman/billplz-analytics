0d11e12d6150da48b0dce02f6e0038ecdb2c7458
safwan <safwanazman017@gmail.com>
2023-10-27 18:27:53 +0800

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,7 +46,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       <meta 
         name="viewport" 
-        content="width=device-width, initial-scale=1.0" 
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no"
       />
     </Head>
       {isLoading ? 
