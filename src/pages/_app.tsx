cfa29469673ec9fe6b8862ebcb15d8efacf381f4
wan <47422904+safwanAzman@users.noreply.github.com>
2023-11-07 19:14:17 +0800

    fix: ad content modules on tailwindcss config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,6 +44,10 @@ export default function App({ Component, pageProps }: AppProps) {
             name="description"
             content="Billplz - Billplz Analytics"
           />
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no"
+        />
       </Head>
       <MobileSidebarProvider>
         {isLoading ? <Loading/>: <Component {...pageProps} />}
